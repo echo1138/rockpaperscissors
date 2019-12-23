6b04333fa5974db5ec9eb941b31c9ca8eb512040
Giselle <giselle.domingo.x@gmail.com>
2019-12-22 21:19:55 -0500

    updated files

--- js-bot.js
+++ js-bot.js
@@ -3,21 +3,51 @@ function computerPlay(){
     let ran=Math.floor(Math.random()*3)+1
     switch(ran){
         case 1:
-            console.log("rokku!")
+            return("rokku!")
             break;
         case 2:
-            console.log("scissors!")
+            return("scissors!")
             break;
         case 3:
-            console.log("paper!")
+            return("paper!");
             break;
     }
 }
+/*
+======= TESTING
+let text=computerPlay()
+console.log(text) */
 
-/* function playRound(playerSelection, computerSelection) {
-	// your code here!
+
+ function playRound(playerSelection, computerSelection) {
+    if (computerSelection == "scissors!" && playerSelection == "rokku!"){
+        return("u win!")
+    }
+    else if (computerSelection == "rokku!" && playerSelection == "scissors!"){
+        return("computer wins!")
+    }
+    else if (computerSelection == "paper!" && playerSelection == "scissor!"){
+        return("computer wins!")
+    }
+    else if (computerSelection == "scissor!" && playerSelection == "paper!"){
+        return("u win!")
+    }
+    else if (computerSelection == "paper!" && playerSelection == "rokku!"){
+        return("computer wins!!")
+    }
+    else if (computerSelection == "rokku!" && playerSelection == "paper!"){
+        return("u win!")
+    }
+    else{
+        return("draw, play again.")
+    }
 }
+function output(){
+    console.log("player: " + playerSelection)
+    console.log("computer: " + computerSelection)
+    console.log(playRound(playerSelection, computerSelection))
+    }
 
-const playerSelection = 'rock'
+const playerSelection="rokku!"
 const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection)) */
+output()
